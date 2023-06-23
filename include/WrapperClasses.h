e466d4f5a621e6608b3c35a58dedd92109dbe396
Linus Henke <linus.henke@mci.edu>
2023-06-23 16:48:21 +0200

    Commented the WrapperClasses.h file.

--- include/WrapperClasses.h
+++ include/WrapperClasses.h
@@ -1,12 +1,15 @@
 /**
- * This file includes definitions of classes wrapped around the plane OpenFHE and NeuralOFHE classes because there were
+ * @file WrapperClasses.h
+ *
+ * @brief This file includes definitions of classes wrapped around the plane OpenFHE and NeuralOFHE classes because there were
  * issues caused by long template arguments and typedefs which where assigned to shared pointers. Each class requires a
  * getter and setter method for setting and getting the shared pointer to the underlying OpenFHE object. The plain
  * OpenFHE will not be directly accessed by the Python front end. Furthermore there will only be an empty constructor
  * that does nothing. The interactions between the classes will be handled by C++ and not within the Python application
  *
  * @author Linus Henke
- * @
+ * Contact: linus.henke@mci.edu
+ *
  */
 #ifndef NEURALPY_WRAPPERCLASSES_H
 #define NEURALPY_WRAPPERCLASSES_H
@@ -20,6 +23,11 @@ typedef CCParams<CryptoContextCKKSRNS> Parameters;
 typedef Ciphertext<DCRTPoly> Cipher;
 typedef CryptoContext<DCRTPoly> Context;
 
+/***
+ * Class used to represent Private and Public keys in Python.
+ *
+ * @tparam T Should be either set to PrivateKey<DCRTPoly> or PublicKey<DCRTPoly>
+ */
 template<class T>
 class PythonKey {
 public:
@@ -55,6 +63,10 @@ private:
 };
 
 
+/***
+ * Class to represent a keypair in Python. Similar to OpenFHE the publicKey and privateKey can be read and written to
+ * from outside the class.
+ */
 class PythonKeypair {
 public:
     PythonKeypair () {}
@@ -74,16 +86,27 @@ public:
 
     void setPlaintext(Plaintext plain) {
         this->pl = plain;
-    };
+    }
 
     Plaintext getPlaintext() {
         return pl;
     }
 
+    /***
+     * Setting the size of the plaintext vector.
+     *
+     * @param length
+     */
     void SetLength (uint32_t length) {
         pl->SetLength(length);
     }
 
+    /***
+     * Getting the plaintext value as a C++ iterator. Conversion between C++ iterators and Python iterators is seamless
+     * with Pybind11.
+     *
+     * @return Plain vector
+     */
     std::vector<double> GetPackedValue () {
         return pl->GetRealPackedValue();
     }
@@ -105,6 +128,11 @@ public:
         return ciphertext;
     }
 
+    /***
+     * Method that allows a ciphertexts to be serialized from a file.
+     *
+     * @param filePath Path to Ciphertext file
+     */
     void load(std::string filePath) {
         if (!Serial::DeserializeFromFile(filePath, ciphertext, SerType::BINARY)) {
             std::cerr << "Could not deserialize " + filePath + " ciphertext" << std::endl;
@@ -113,6 +141,11 @@ public:
         std::cout << "Ciphertext " + filePath << " deserialized." << std::endl;
     }
 
+    /***
+     * Method that allows serialization of a ciphertext.
+     *
+     * @param filePath
+     */
     void save(std::string filePath) {
         if(!Serial::SerializeToFile(filePath, ciphertext, SerType::BINARY)) {
             std::cerr << "Error Serializing ciphertext." << std::endl;
@@ -138,25 +171,54 @@ public:
         context->Enable(feature);
     }
 
+    /***
+     * Method that generates multiplication keys within the context object.
+     *
+     * @param privateKey Mult. keys are generated from the private key
+     */
     void EvalMultKeyGen (PythonKey<PrivateKey<DCRTPoly>> privateKey) {
         context->EvalMultKeyGen(privateKey.getKey());
     }
 
+    /***
+     * Generate rotation keys required to do matrix multiplication with the contexts batch size.
+     *
+     * @param key
+     */
     void GenRotations (PythonKey<PrivateKey<DCRTPoly>> key) {
         std::vector<int> rotations = GetRotations(context->GetEncodingParams()->GetBatchSize());
         context->EvalRotateKeyGen(key.getKey(), rotations);
     }
 
+    /***
+     * Get dimension of the polynomial ring within the context.
+     *
+     * @return Ring dimension
+     */
     uint32_t GetRingDim() {
         return context->GetRingDimension();
     }
 
+    /***
+     * Encrypt a plaintext using the public key.
+     *
+     * @param plaintext Plaintext that should be encrypted.
+     * @param publicKey Public key of the application.
+     * @return Encrypted ciphertext.
+     */
     PythonCiphertext Encrypt(PythonPlaintext plaintext, PythonKey<PublicKey<DCRTPoly>> publicKey) {
         PythonCiphertext result;
         result.setCiphertext(context->Encrypt(publicKey.getKey(), plaintext.getPlaintext()));
         return result;
     }
 
+    /***
+     * Decrypt a ciphertext using the private key.
+     *
+     * @param cipher Ciphertext that should be decrypted.
+     * @param privateKey Private key of the application.
+     * @return Plaintext object resulting from the encryption.
+     */
     PythonPlaintext Decrypt(PythonCiphertext cipher, PythonKey<PrivateKey<DCRTPoly>> privateKey) {
         PythonPlaintext result;
         Plaintext pl;
@@ -167,12 +229,23 @@ public:
         return result;
     }
 
+    /***
+     * Packing a C++ iterator containing doubles into a plaintext object.
+     *
+     * @param plaintext Plaintext in form of a C++ iterator
+     * @return Plaintext object
+     */
     PythonPlaintext PackPlaintext(std::vector<double> plaintext) {
         PythonPlaintext result;
         result.setPlaintext(context->MakeCKKSPackedPlaintext(plaintext));
         return result;
     }
 
+    /***
+     * Generate keypair for public key encryption.
+     *
+     * @return Keypair object
+     */
     PythonKeypair KeyGen () {
         PythonKeypair keys;
         auto keyPair = context->KeyGen();
@@ -187,6 +260,11 @@ public:
         return context;
     }
 
+    /***
+     * Load context object from file.
+     *
+     * @param filePath
+     */
     void load(std::string filePath) {
         if (!Serial::DeserializeFromFile(filePath, context, SerType::BINARY)) {
             std::cerr << "Error loading context" << std::endl;
@@ -195,6 +273,11 @@ public:
         std::cout << "Context has been loaded." << std::endl;
     }
 
+    /***
+     * Load multiplication keys from file into the context object.
+     *
+     * @param filePath
+     */
     void loadMultKeys(std::string filePath) {
         context->ClearEvalMultKeys();
 
@@ -211,6 +294,11 @@ public:
         multKeyIStream.close();
     }
 
+    /***
+     * Load rotation keys from file into the context object.
+     *
+     * @param filePath
+     */
     void loadRotKeys (std::string filePath) {
         context->ClearEvalAutomorphismKeys();
 
@@ -227,6 +315,11 @@ public:
         rotKeyIStream.close();
     }
 
+    /***
+     * Serialize context object without keys to file.
+     *
+     * @param filePath
+     */
     void save(std::string filePath) {
         if (!Serial::SerializeToFile(filePath, context, SerType::BINARY)) {
             std::cerr << "Error serializing context." << std::endl;
@@ -235,6 +328,11 @@ public:
         std::cout << "Cryptocontext serialized!" << std::endl;
     }
 
+    /***
+     * Serialize multiplication keys to file.
+     *
+     * @param filePath
+     */
     void saveMultKeys(std::string filePath) {
         std::ofstream multKeyFile(filePath, std::ios::out | std::ios::binary);
         if (multKeyFile.is_open()) {
@@ -250,6 +348,11 @@ public:
         }
     }
 
+    /***
+     * Serialize rotation keys to file.
+     *
+     * @param filePath
+     */
     void saveRotKeys(std::string filePath) {
         std::ofstream rotKeyFile(filePath, std::ios::out | std::ios::binary);
         if (rotKeyFile.is_open()) {
@@ -268,7 +371,9 @@ private:
     Context context;
 };
 
-
+/***
+ * Trampoline class for the Operator base class so that Pybind11 can handle virtual functions.
+ */
 class PythonOperator : public Operator {
 public:
     using Operator::Operator;
@@ -283,7 +388,11 @@ public:
     }
 };
 
-
+/***
+ * Template class that overrides the forward method for the inherited class in the template class.
+ *
+ * @tparam Impl Inherited class
+ */
 template <class Impl> class PyImpl : public Impl {
 public:
     using Impl::Impl;
@@ -293,7 +402,10 @@ public:
     }
 };
 
-
+/***
+ * Implementation of the activation function class that overrides the forward function. There is no requirement to
+ * create a trampoline for the getFunc method since this is a protected method.
+ */
 class PythonActivation : public ActivationFunction {
 public:
     using ActivationFunction::ActivationFunction;
